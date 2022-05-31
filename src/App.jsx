48d0559ad7c10c754fe5eb3e649185cd319e4248
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-05-31 18:26:44 +0700

    feat(App): basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
+import {
+  Col,
+  Container,
+  Form,
+  FormControl,
+  Row,
+  Button,
+  Card,
+} from "react-bootstrap";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  )
+    <Container>
+      <Row className="mt-3">
+        <Col sm={12} lg={4}>
+          <h1>Bookist</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="textInput">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder="Enter title" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="authorInput">
+              <Form.Label>Author</Form.Label>
+              <Form.Control type="text" placeholder="Enter Author" />
+            </Form.Group>
+            <Button variant="primary" className="w-100">
+              Insert Book
+            </Button>
+          </Form>
+        </Col>
+        <Col sm={12} lg={8} className="mt-3 mt-lg-0">
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>How to learn Redux</Card.Title>
+              <Card.Text
+                className="fst-italic"
+                style={{
+                  color: "#B1B1B1",
+                }}
+              >
+                Albert
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="d-flex">
+              <Button variant="danger" className="ms-auto">
+                Delete
+              </Button>
+              <Button variant="primary" className="ms-2">
+                Update
+              </Button>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default App
+export default App;
